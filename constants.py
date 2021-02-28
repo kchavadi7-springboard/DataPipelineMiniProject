46f6f618daf161c6365539e360dce46836f27b02
kchavadi7-springboard <74509329+kchavadi7-springboard@users.noreply.github.com>
2021-02-28 13:10:04 -0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -7,11 +7,11 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 CSV_FILE_PATH = os.path.join(ROOT_DIR, 'third_party_sales_1.csv')
 # print(CSV_FILE_PATH)
 
-USER = 'root'
-PASSWORD = 'rootadmin'
-HOST = 'localhost'
-PORT = '3306'
-DATABASE = 'Tickets'
+USER = ''
+PASSWORD = ''
+HOST = ''
+PORT = ''
+DATABASE = ''
 
 
 
